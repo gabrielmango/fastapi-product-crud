b5092df50f0302880e5532c278eaa4cf4d1221bc
Gabriel Mango <gabrielhmango@gmail.com>
2024-11-20 13:01:59 -0300

    feat: create write_data function for write a data to projects.json file

--- app/database.py
+++ app/database.py
@@ -11,9 +11,28 @@ def read_data():
             return json.load(f)
     return []
 
+def write_data(data):
+    """ Write the given data to the JSON file. """
+    with open(DB_PATH, "w", encoding="utf-8") as f:
+        json.dump(data, f, indent=4)
+
 
 if __name__ == "__main__":
+    
     data = read_data()
     print("Current products:")
     for product in data:
         print(f"{product['id']}: {product['name']} - ${product['price']}")
+    
+    new_product = {
+        "id": 2,
+        "name": "New Product",
+        "description": "This is a new product.",
+        "price": 9.99
+    }
+    
+    data.append(new_product)
+    write_data(data)
+    
+    print("\nNew product added:")
+    print(f"{new_product['id']}: {new_product['name']} - ${new_product['price']}")
