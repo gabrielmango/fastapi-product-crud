2f05c071d7f1eeb60ffe18c5c07273f0fac17e6b
Gabriel Mango <gabrielhmango@gmail.com>
2024-11-20 13:32:37 -0300

    feat: create endpoint read_product_by_id

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
-from fastapi import FastAPI
-from app.crud import get_all_products
+from fastapi import FastAPI, HTTPException
+from app.crud import get_all_products, get_product_by_id
 
 
 app = FastAPI()
@@ -7,3 +7,10 @@ app = FastAPI()
 @app.get("/products/")
 def read_products():
     return get_all_products()
+
+@app.get("/products/{product_id}")
+def read_product_by_id(product_id: int):
+    product = get_product_by_id(product_id)
+    if not product:
+        raise HTTPException(status_code=404, detail="Product not found")
+    return product
