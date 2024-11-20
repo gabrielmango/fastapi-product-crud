a48d42812e03931292c7114e3e9195caef394707
Gabriel Mango <gabrielhmango@gmail.com>
2024-11-20 13:43:00 -0300

    feat: create endpoint delete_existing_product

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, HTTPException
-from app.crud import get_all_products, get_product_by_id, create_product, update_product
+from app.crud import get_all_products, get_product_by_id, create_product, update_product, delete_product
 from app.schemas import ProductCreate
 
 
@@ -26,3 +26,10 @@ def update_existing_product(product_id: int, product: ProductCreate):
     if not updated:
         raise HTTPException(status_code=404, detail="Product not found")
     return updated
+
+@app.delete("/products/{product_id}")
+def delete_existing_product(product_id: int):
+    deleted = delete_product(product_id)
+    if not deleted:
+        raise HTTPException(status_code=404, detail="Product not found")
+    return {"message": "Product deleted successfully"}
