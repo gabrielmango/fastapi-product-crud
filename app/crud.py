697b3beb31c490031a48480274994a67b589dd47
Gabriel Mango <gabrielhmango@gmail.com>
2024-11-20 13:09:51 -0300

    feat: create get_product_by_id for return one data from products.json

--- app/crud.py
+++ app/crud.py
@@ -3,7 +3,14 @@ from database import read_data
 def get_all_products():
     return read_data()
 
+def get_product_by_id(product_id: int):
+    products = read_data()
+    return next((p for p in products if p["id"] == product_id), None)
+
 if __name__ == '__main__':
-    products = get_all_products()
-    for product in products:
-        print(product)
+    product_id = 1
+    product = get_product_by_id(product_id)
+    if product:
+        print(f"\nProduct with ID {product_id}:")
+        print(f"Name: {product['name']}, Price: {product['price']}")
+    
